6d21b6a0beb57f2ea94734d794d0c345d81bbd63
Mindaugas <vardas@pavarde.lt>
2018-12-14 14:18:47 +0200

    update src

--- src/veiksmai/FormuleKortuMacui.java
+++
@@ -1,9 +0,0 @@
-package veiksmai;
-
-import pirminiaiduomenys.KortuVertes;
-
-import java.util.List;
-
-public interface FormuleKortuMacui {
-    int[] formuleKortai(String player1, String player2, List<KortuVertes> p1, List<KortuVertes> p2, int statymas1, int statymas2);
-}
